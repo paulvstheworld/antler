8dd294eb2da6cae1dcbcdcbb9bf17454a3721fbc
Paul <paulvstheworld@gmail.com>
2013-09-26 11:44:41 -0400

    adding skeleton socket handling

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var http = require('http')
   , express = require('express')
   , hbs = require('hbs')
   , Sequelize = require('sequelize')
+  , cookie = require('cookie')
   
   // servers
   , app = express()
@@ -30,6 +31,10 @@ var http = require('http')
   , sessionHelpers = require(__dirname + '/lib/helpers/session.js')
   , templateHelpers = require(__dirname + '/lib/helpers/template.js')
   , LoginValidator = require(__dirname + '/lib/validators/LoginValidator.js')
+
+
+var INACTIVE_TIMEOUT = 1000 * 30; // 30 secs
+var usersBySocket = {};
 
 
 app.configure(function() {
@@ -62,8 +67,6 @@ app.get('/', function(req, res, next) {
   else {
     return res.redirect('/login');
   }
-
-  
 });
 
 app.get('/login', function(req, res) {
@@ -136,12 +139,86 @@ app.post('/login', function(req, res) {
 });
 
 
+
+
+// save session id to socket
+io.set('authorization', function (data, accept) {
+  // check if there's a cookie header
+  if (data.headers.cookie) {
+    // if there is, parse the cookie
+    data.cookie = cookie.parse(data.headers.cookie);
+    // note that you will need to use the same key to grad the
+    // session id, as you specified in the Express setup.
+    data.sessionID = data.cookie['sessionid'];
+  } 
+  else {
+   // if there isn't, turn down the connection with a message
+   // and leave the function.
+   return accept('No cookie transmitted.', false);
+  }
+  
+  // accept the incoming connection
+  accept(null, true);
+});
+
+
 io.sockets.on('connection', function(socket) {
-  socket.on('disconnect', function() {
-    socket.broadcast('user.left', {
-      msg: 'user left'
+  var sessionid = socket.handshake.sessionID;
+  
+  User.find({where: {sessionid: sessionid}})
+    .success(function(user) {
+      usersBySocket[socket.id] = user;
+      socket.emit('user.active', user.dataValues);
+      socket.broadcast.emit('guest.active', user.dataValues);
+      
+      setTimeout(function() {
+        socket.emit('user.inactive', user.dataValues);
+        socket.broadcast.emit('guest.inactive', user.dataValues);
+      }, INACTIVE_TIMEOUT);
     });
+  
+  socket.on('disconnect', function() {
+    var name = '';
+    var user = usersBySocket[this.id];
+    
+    if(user) {
+      name = [user.firstname, user.lastname ].join(' ');
+      this.broadcast.emit('guest.disconnected', user);
+      
+      console.log('sessionid = ' + name + ' has left');
+      delete usersBySocket[this.id];
+    }
   });
+  
+  // TODO finish this
+  socket.on('connect.request', function(data) {
+    var user = usersBySocket[this.id];
+    var guestID = data.id
+    if(user) {
+      console.log('user id=' + user.id + ' requests to be connected with ' + guestID);
+      
+      
+      //look up guest user
+      User.find({where: {id:guestID}})
+        .success(function(guestUser) {
+          if(guestUser) {
+            console.log('found guest user');
+            // check if connect already exists (either forward or reverse connection)
+              // if there isn't a connection, send confirmation request to guest
+              // else notify the requested user that a conncetion has already been made
+          }
+          else {
+            // TODO -- tell requested user that the guest user no longer exists
+            console.log('notify user that guest no longer exists'); 
+          }
+        });
+      
+      // check if connect already exists (either forward or reverse connection)
+      
+      // if there isn't a connection requests to be saved
+      // else notify the requested user that a connection has already been made
+    }
+  })
 });
 
 
