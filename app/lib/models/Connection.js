7ba1d785639711e881a1948c8c953b111d065352
Paul <paulvstheworld@gmail.com>
2013-09-26 17:16:30 -0400

    removing old connection and user sequelize models

--- app/lib/models/Connection.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Connection', {
-    id: DataTypes.INTEGER,
-    user_src_id: DataTypes.INTEGER,
-    user_dest_id: DataTypes.INTEGER,
-    createdAt: DataTypes.INTEGER,
-    updatedAt: DataTypes.INTEGER
-  })
-}
